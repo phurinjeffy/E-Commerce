d264a535b0122ac200f5b0a82356bbc0fe3cf49f
Phurin Vanasrivilai <phurinjeffyjj@gmail.com>
2024-01-09 10:35:36 +0700

    update

---
+++ components/Navbar.tsx
@@ -0,0 +1,67 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const Navbar = () => {
+  return (
+    <header className="w-full fixed z-10 bg-white border-b-[0.5px] border-opacity-50">
+      <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4">
+        <div className="flex items-center gap-8">
+          <Link href="/">
+            <Image 
+              src="/uniqlo.png"
+              alt="Logo"
+              width={90}
+              height={9}
+              className="object-contain"
+            />
+          </Link>
+          <Link href="/" className="font-bold">WOMEN</Link>
+          <Link href="/" className="font-bold">MEN</Link>
+          <Link href="/" className="font-bold">KIDS</Link>
+          <Link href="/" className="font-bold">BABY</Link>
+        </div>
+
+        <div className="flex items-center gap-8">
+          <Link href="/">
+            <Image 
+              src="/search.svg"
+              alt="Logo"
+              width={25}
+              height={25}
+              className="object-contain"
+            />
+          </Link>
+          <Link href="/">
+            <Image 
+              src="/user.svg"
+              alt="Logo"
+              width={25}
+              height={25}
+              className="object-contain"
+            />
+          </Link>
+          <Link href="/">
+            <Image 
+              src="/heart.svg"
+              alt="Logo"
+              width={25}
+              height={25}
+              className="object-contain"
+            />
+          </Link>
+          <Link href="/">
+            <Image 
+              src="/cart.svg"
+              alt="Logo"
+              width={25}
+              height={25}
+              className="object-contain"
+            />
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
---
+++ constants/index.ts
@@ -0,0 +1,31 @@
+export const footerLinks = [
+  {
+    title: "About",
+    links: [
+      { title: "Information", url: "/" },
+      { title: "Store Locator", url: "/" },
+      { title: "Career", url: "/" },
+      { title: "Sustainability", url: "/" },
+    ],
+  },
+  {
+    title: "Help",
+    links: [
+      { title: "FAQ", url: "/" },
+      { title: "Return Policy", url: "/" },
+      { title: "Privacy Policy", url: "/" },
+      { title: "Terms of Use", url: "/" },
+      { title: "Accessibility", url: "/" },
+      { title: "Bulk Order", url: "/" },
+    ],
+  },
+  {
+    title: "Social Account",
+    links: [
+      { title: "Instagram", url: "/" },
+      { title: "Twitter", url: "/" },
+      { title: "Facebook", url: "/" },
+      { title: "LINE", url: "/" },
+    ],
+  },
+];
