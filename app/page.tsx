9692c139ea8a4204a1788d3b4924c8a65ddc7c9a
Phurin Vanasrivilai <phurinjeffyjj@gmail.com>
2024-01-10 23:16:55 +0700

    update homepage constants

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,43 @@
-"use client";
 import React from "react";
 import Image from "next/image";
 
 export default function Home() {
+  const notices = [
+    {
+      text: "UNIQLO GATEWAY BANGSUE WILL BE CLOSED ON JANUARY 15, 2024. READMORE FOR DETAILS.",
+    },
+    {
+      text: "JOIN UNIQLO'S FREE PRODUCT SAMPLING PROGRAM. GET A CHANCE TO WIN U CREW NECK SHORT SLEEVE T-SHIRT (100 PRIZES), CLICK!",
+    },
+  ];
+
+  const benefits = [
+    {
+      image: "/benefits1.jpeg",
+      title: "SAME DAY CLICK & COLLECT",
+      description:
+        "Get order at the store in the same day when select Click & Collect before 11 AM. *Only selected stores",
+    },
+    {
+      image: "/benefits2.jpeg",
+      title: "EXTRA SIZES FROM XS - 3XL",
+      description:
+        "You can find a wide range of extra size from XS - 3XL and 100cm for kids at online store.",
+    },
+    {
+      image: "/benefits3.jpeg",
+      title: "CHECK INVENTORY",
+      description:
+        "Scan barcodes to check your preferred color and size inventory, more product detail, extra size and more.",
+    },
+    {
+      image: "/benefits4.jpeg",
+      title: "EARLY ACCESS",
+      description:
+        "Get early access to new arrivals and special collections. Stay ahead of the fashion trends!",
+    },
+  ];
+
   return (
     <>
       <div
@@ -27,37 +62,29 @@ export default function Home() {
           <div className="text-4xl font-extrabold">NOTICE</div>
           <div className="my-4">
             <ul className="flex flex-col gap-1 text-base list-disc">
-              <li>
-                UNIQLO GATEWAY BANGSUE WILL BE CLOSED ON JANUARY 15, 2024. READ
-                MORE FOR DETAILS.
-              </li>
-              <li>
-                JOIN UNIQLO'S FREE PRODUCT SAMPLING PROGRAM. GET A CHANCE TO WIN
-                U CREW NECK SHORT SLEEVE T-SHIRT (100 PRIZES), CLICK!
-              </li>
+              {notices.map((notice) => (
+                <li>{notice.text}</li>
+              ))}
             </ul>
           </div>
         </div>
 
         <div className="my-6">
           <div className="text-4xl font-extrabold">UNIQLO APP BENEFITS</div>
-          <div className="flex flex-row my-4">
-            <div className="flex flex-col w-[300px] gap-2">
-              <Image
-                src="/benefits1.jpeg"
-                width={300}
-                height={300}
-                alt="Benefits1"
-                className="my-2"
-              />
-              <div className="text-base font-extrabold">
-                SAME DAY CLICK & COLLECT
+          <div className="flex flex-row my-4 gap-6">
+            {benefits.map((benefit, index) => (
+              <div key={index} className="flex flex-col w-[300px] gap-2">
+                <Image
+                  src={benefit.image}
+                  width={300}
+                  height={300}
+                  alt={`Benefit ${index + 1}`}
+                  className="my-2"
+                />
+                <div className="text-base font-extrabold">{benefit.title}</div>
+                <div className="text-base">{benefit.description}</div>
               </div>
-              <div className="text-sm">
-                Be the first to know when styles you want are available by
-                becoming an app member and turn on notification.
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
